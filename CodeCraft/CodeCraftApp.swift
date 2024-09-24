21f3ea25091cce7adfa76fac5561e703e8db006c
giovannamoeller <giovannagmoeller@gmail.com>
2024-09-24 10:31:24 -0300

    Initial Commit

---
+++ CodeCraft/CodeCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeCraftApp.swift
+//  CodeCraft
+//
+//  Created by Giovanna Moeller on 24/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeCraftTests/CodeCraftTests.swift
@@ -0,0 +1,17 @@
+//
+//  CodeCraftTests.swift
+//  CodeCraftTests
+//
+//  Created by Giovanna Moeller on 24/09/24.
+//
+
+import Testing
+@testable import CodeCraft
+
+struct CodeCraftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
