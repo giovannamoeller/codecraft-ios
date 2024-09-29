bd91f3a21e65608798d751a979d5f6bf0d6d91d9
giovannamoeller <giovannagmoeller@gmail.com>
2024-09-29 14:19:49 -0300

    feat: create interactive singly linked list view

---
+++ CodeCraft/Components/CCSinglyNodeView.swift
@@ -0,0 +1,49 @@
+//
+//  CCSinglyNodeView.swift
+//  CodeCraft
+//
+//  Created by Giovanna Moeller on 27/09/24.
+//
+
+import SwiftUI
+
+struct CCSinglyNodeView: View {
+    let element: Int
+    let isHead: Bool
+    let isTail: Bool
+    
+    init(element: Int,
+         isHead: Bool = false,
+         isTail: Bool = false) {
+        self.element = element
+        self.isHead = isHead
+        self.isTail = isTail
+    }
+    
+    private var headOrTailText: String {
+        isHead && isTail ? "Head/Tail" : (isHead ? "Head" : (isTail ? "Tail" : ""))
+    }
+    
+    var body: some View {
+        HStack {
+            ZStack {
+                Circle()
+                    .fill(AppTheme.Colors.lightLavender)
+                    .frame(width: 64, height: 64)
+                Text("\(element)")
+                    .appFont(AppTheme.Fonts.title3)
+                Text(headOrTailText)
+                    .foregroundStyle(.white)
+                    .offset(y: -52)
+            }
+            if !isTail {
+                Image(systemName: "arrow.forward")
+                    .foregroundStyle(.white)
+            }
+        }
+    }
+}
+
+#Preview {
+    CCSinglyNodeView(element: 10)
+}
--- CodeCraft/Views/InteractiveViews/InteractiveSinglyLinkedListView.swift
+++ CodeCraft/Views/InteractiveViews/InteractiveSinglyLinkedListView.swift
@@ -12,14 +12,14 @@ enum LinkedListAction {
 }
 
 struct InteractiveSinglyLinkedListView: View {
-    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
-    @State private var maxHeight: CGFloat = 0
-    
-    @State private var lastAction: LinkedListAction = .none
-    @State private var showInstructions = false
-    @State private var animatingNodeIndex: Int?
-    
     @StateObject private var linkedList = CCSinglyLinkedList<Int>()
+    @State private var animatingNodeIndex: Int = -1
+    @State private var lastAction: LinkedListAction = .none
+    @State private var newElement: Int?
+    @State private var isAnimating: Bool = false
+    @State private var isButtonDisabled: Bool = false
+    @State private var isRemovingElement: Bool = false
+    @State private var removedElement: Int?
     
     let analogies: [Analogy] = [
         Analogy(icon: "link", title: "Node-based", description: "Linked lists consist of nodes, each containing data and a reference to the next node."),
@@ -28,72 +28,128 @@ struct InteractiveSinglyLinkedListView: View {
         Analogy(icon: "bolt", title: "Efficient insertions", description: "Inserting elements at the beginning or end is very efficient (O(1) time)."),
     ]
     
-    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
-    
     private func getRandomElement() -> Int {
         return Int.random(in: 0..<100)
     }
     
     private func insertFirst() {
-        withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.3)) {
-            lastAction = .insertFirst
-            linkedList.insertAtHead(getRandomElement())
-            animatingNodeIndex = 0
+        isButtonDisabled = true
+        lastAction = .insertFirst
+        let elementToInsert = getRandomElement()
+        
+        withAnimation(.spring(duration: 0.5, bounce: 0.3)) {
+            newElement = elementToInsert
         }
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
-            withAnimation(.spring()) {
-                lastAction = .none
-                animatingNodeIndex = nil
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            withAnimation(.spring(duration: 0.5)) {
+                isAnimating = true
+                animatingNodeIndex = 0
             }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            animatingNodeIndex = -1
+            isAnimating = false
+            linkedList.insertAtHead(elementToInsert)
+            newElement = nil
+            withAnimation {
+                isButtonDisabled = false
+            }
+            lastAction = .none
         }
     }
     
     private func insertLast() {
-        withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.3)) {
-            lastAction = .insertLast
-            linkedList.insertAtTail(getRandomElement())
-            animatingNodeIndex = linkedList.length - 1
+        isButtonDisabled = true
+        lastAction = .insertLast
+        let elementToInsert = getRandomElement()
+        
+        withAnimation(.spring(duration: 0.5, bounce: 0.3)) {
+            newElement = elementToInsert
         }
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
-            withAnimation(.spring()) {
-                lastAction = .none
-                animatingNodeIndex = nil
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            withAnimation(.spring(duration: 0.5)) {
+                isAnimating = true
+                animatingNodeIndex = linkedList.length
             }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.25) {
+            animatingNodeIndex = -1
+            isAnimating = false
+            linkedList.insertAtTail(elementToInsert)
+            newElement = nil
+            withAnimation {
+                isButtonDisabled = false
+            }
+            lastAction = .none
         }
     }
     
     private func removeFirst() {
         guard !linkedList.isEmpty else { return }
+        isButtonDisabled = true
+        lastAction = .removeFirst
+        animatingNodeIndex = 0
         
-        withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.3)) {
-            lastAction = .removeFirst
-            animatingNodeIndex = 0
+        // Start the animation for removing the first element
+        withAnimation(.easeInOut(duration: 0.5)) {
+            isAnimating = true
+            isRemovingElement = true
+            removedElement = linkedList.head?.value
         }
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
-            let _ = linkedList.removeAtHead()
-            withAnimation(.spring()) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            // Remove the element after a delay
+            
+            withAnimation {
+                _ = linkedList.removeAtHead()
+            }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            withAnimation {
+                animatingNodeIndex = -1
+                isAnimating = false
+                isButtonDisabled = false
                 lastAction = .none
-                animatingNodeIndex = nil
+                isRemovingElement = false
+                removedElement = nil
+                
             }
         }
     }
     
+    
     private func removeLast() {
         guard !linkedList.isEmpty else { return }
+        isButtonDisabled = true
+        lastAction = .removeLast
+        animatingNodeIndex = linkedList.length - 1
         
-        withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.3)) {
-            lastAction = .removeLast
-            animatingNodeIndex = linkedList.length - 1
+        
+        withAnimation(.easeInOut(duration: 0.5)) {
+            isAnimating = true
+            isRemovingElement = true
+            removedElement = linkedList.tail?.value
         }
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
-            let _ = linkedList.removeAtTail()
-            withAnimation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.3)) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            withAnimation(.easeInOut(duration: 0.3)) {
+                let _ = linkedList.removeAtTail()
+                isAnimating = false
+                isRemovingElement = false
+                animatingNodeIndex = -1
+            }
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            withAnimation {
+                isButtonDisabled = false
                 lastAction = .none
-                animatingNodeIndex = nil
+                removedElement = nil
             }
         }
     }
@@ -104,73 +160,99 @@ struct InteractiveSinglyLinkedListView: View {
                 Text("Visualize how a singly linked list works")
                     .frame(maxWidth: .infinity, alignment: .leading)
                     .appFont(AppTheme.Fonts.largeTitle)
-                    .padding(.vertical)
+                    .padding(32)
                 
                 CCFlexibleGridView(data: analogies)
+                    .padding()
                 
                 Text("Observe how the linked list changes as you perform operations below.")
                     .appFont(AppTheme.Fonts.bodyRegular)
-                    .padding(.top)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
                 
-                if linkedList.isEmpty {
+                if linkedList.isEmpty && newElement == nil {
                     Text("Linked list is empty!")
                         .appFont(AppTheme.Fonts.bodyBold)
                         .padding(.vertical)
                 } else {
                     ScrollView(.horizontal, showsIndicators: false) {
                         HStack(spacing: 8.0) {
-                            ForEach(Array(linkedList.toArray().enumerated()), id: \.0) { index, element in
-                                HStack(spacing: 4.0) {
-                                    ZStack {
-                                        Circle()
-                                            .fill(AppTheme.Colors.lightLavender)
-                                            .frame(width: 64, height: 64)
-                                        Text("\(element)")
-                                            .appFont(AppTheme.Fonts.title3)
-                                    }
-                                    if index < linkedList.length - 1 {
-                                        Image(systemName: "arrow.right")
-                                            .foregroundColor(AppTheme.Colors.indigo)
-                                    }
-                                }
-                                .opacity(animatingNodeIndex == index ? 0.5 : 1)
-                                .scaleEffect(animatingNodeIndex == index ? 0.8 : 1)
-                                .offset(x: lastAction == .insertFirst && index == 0 ? 70 : 0)
-                                .offset(x: lastAction == .insertLast && index == linkedList.length - 1 ? -70 : 0)
-                                .offset(y: (lastAction == .removeFirst && index == 0) || (lastAction == .removeLast && index == linkedList.length - 1) ? 50 : 0)
+                            if newElement != nil && lastAction == .insertFirst {
+                                CCSinglyNodeView(
+                                    element: newElement!,
+                                    isHead: true,
+                                    isTail: linkedList.isEmpty
+                                )
+                                .opacity(isAnimating ? 1 : 0)
+                                .scaleEffect(isAnimating ? 1 : 0.5)
+                                .offset(y: isAnimating ? 0 : -50)
+                                .animation(.spring(bounce: 0.5), value: isAnimating)
+                            }
+                            
+                            ForEach(Array(linkedList.toArray().enumerated()), id: \.element) { index, element in
+                                CCSinglyNodeView(
+                                    element: element,
+                                    isHead: index == 0 && (lastAction != .insertFirst || !isAnimating),
+                                    isTail: index == linkedList.length - 1 && (lastAction != .insertLast || !isAnimating)
+                                )
+                                .opacity(removedElement == element ? 0 : 1)
+                                .scaleEffect(removedElement == element ? 0.5 : 1)
+                                .offset(y: removedElement == element ? -50 : 0)
+                            }
+                            .animation(.spring(bounce: 0.5), value: removedElement)
+                            //.transition(.asymmetric(insertion: .scale, removal: .opacity))
+                            
+                            if newElement != nil && lastAction == .insertLast {
+                                CCSinglyNodeView(
+                                    element: newElement!,
+                                    isHead: linkedList.isEmpty,
+                                    isTail: true
+                                )
+                                .opacity(isAnimating ? 1 : 0)
+                                .scaleEffect(isAnimating ? 1 : 0.5)
+                                .offset(y: isAnimating ? 0 : -50)
+                                .animation(.spring(bounce: 0.5), value: isAnimating)
                             }
                         }
-                        .frame(minHeight: 164)
-                        .padding()
+                        .padding(.horizontal, 32)
+                        .padding(.vertical, 32)
+                        .padding(.top)
                     }
-                    .padding(.vertical, 32)
-                    .animation(.spring(response: 0.5, dampingFraction: 0.7, blendDuration: 0.3), value: linkedList.length)
+                    .background(AppTheme.Colors.indigo)
+                    .padding(.vertical)
                 }
                 
-                VStack(spacing: 15) {
-                    HStack {
-                        Button(action: insertFirst) {
-                            CCSecondaryButtonView(text: "Insert First")
-                        }
-                        Spacer()
-                        Button(action: insertLast) {
-                            CCSecondaryButtonView(text: "Insert Last")
-                        }
+                HStack {
+                    Button(action: insertFirst) {
+                        CCSecondaryButtonView(text: "Insert First")
                     }
-                    .frame(maxWidth: 320)
-                    //.background(Color.red)
-                    HStack {
-                        Button(action: removeFirst) {
-                            CCSecondaryButtonView(text: "Remove First")
-                        }
-                        Spacer()
-                        Button(action: removeLast) {
-                            CCSecondaryButtonView(text: "Remove Last")
-                        }
+                    .disabled(isButtonDisabled)
+                    .opacity(isButtonDisabled ? 0.5 : 1)
+                    
+                    Button(action: insertLast) {
+                        CCSecondaryButtonView(text: "Insert Last")
                     }
-                    .frame(maxWidth: 320)
-                    //.background(Color.blue)
+                    .disabled(isButtonDisabled)
+                    .opacity(isButtonDisabled ? 0.5 : 1)
                 }
+                .frame(maxWidth: 320)
+                
+                HStack {
+                    Button(action: removeFirst) {
+                        CCSecondaryButtonView(text: "Remove First")
+                    }
+                    .disabled(isButtonDisabled || linkedList.isEmpty)
+                    .opacity(isButtonDisabled || linkedList.isEmpty ? 0.5 : 1)
+                    
+                    Button(action: removeLast) {
+                        CCSecondaryButtonView(text: "Remove Last")
+                    }
+                    
+                    .disabled(isButtonDisabled || linkedList.isEmpty)
+                    .opacity(isButtonDisabled || linkedList.isEmpty ? 0.5 : 1)
+                    
+                }
+                .frame(maxWidth: 320)
                 
                 NavigationLink {
                     //LinkedListCodeCheatsheetView()
@@ -179,20 +261,11 @@ struct InteractiveSinglyLinkedListView: View {
                         .padding(.top, 32)
                 }
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button(action: {
-                        withAnimation {
-                            showInstructions = true
-                        }
-                    }) {
-                        Image(systemName: "ellipsis")
-                            .foregroundStyle(AppTheme.Colors.mediumLavender)
-                            .rotationEffect(.degrees(90))
-                    }
-                }
+        }
+        .onAppear {
+            for _ in 0..<3 {
+                linkedList.insertAtHead(getRandomElement())
             }
-            .padding(32)
         }
     }
 }
